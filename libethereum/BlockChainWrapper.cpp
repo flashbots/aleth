6f5c4aced419e8e2e8179d0f352515235ca7cbd1
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2021-08-16 08:26:39 -0400

    Other changes

--- libethereum/BlockChainWrapper.cpp
+++ libethereum/BlockChainWrapper.cpp
@@ -1,5 +1,50 @@
 #include "BlockChainWrapper.h"
 
+#pragma once
+
+#include "Account.h"
+#include "BlockDetails.h"
+#include "BlockQueue.h"
+#include "ChainParams.h"
+#include "DatabasePaths.h"
+#include "LastBlockHashesFace.h"
+#include "State.h"
+#include "Transaction.h"
+#include "VerifiedBlock.h"
+#include <libdevcore/Exceptions.h>
+#include <libdevcore/Guards.h>
+#include <libdevcore/Log.h>
+#include <libdevcore/db.h>
+#include <libethcore/BlockHeader.h>
+#include <libethcore/Common.h>
+#include <libethcore/SealEngine.h>
+#include <boost/filesystem/path.hpp>
+#include <chrono>
+#include <deque>
+#include <unordered_map>
+#include <unordered_set>
+
+namespace dev
+{
+class OverlayDB;
+
+namespace eth
+{
+class State;
+class Block;
+class ImportPerformanceLogger;
+
+using BlocksHash = std::unordered_map<h256, bytes>;
+using TransactionHashes = h256s;
+using UncleHashes = h256s;
+
+using ProgressCallback = std::function<void(unsigned, unsigned)>;
+
+/**
+ * @brief Implements the blockchain database. All data this gives is disk-backed.
+ * @threadsafe
+ */
+
 BlockChainWrapper::BlockChainWrapper(OverlayDB state)
 {
     // Set last_block_hashes
@@ -14,7 +59,7 @@ LastBlockHashesFace lastBlockHashes()
 /// parent + 1, ... parent + @a _generations).
 /// @returns set including the header-hash of every parent (including @a _parent) up to and
 /// including generation + @a _generations together with all their quoted uncles.
-h256Hash BlockChainWrapper::allKinFrom(h256 const& _parent, unsigned _generations) const {}
+h256 BlockChainWrapper::allKinFrom(h256 const& _parent, unsigned _generations) const {}
 
 bool BlockChainWrapper::isKnown(h256 const& hash)
 {
@@ -23,6 +68,9 @@ bool BlockChainWrapper::isKnown(h256 const& hash)
 
 bytes BlockChainWrapper::block(h256 const& _hash) const {}
 
-h256 BlockChainWrapper::parentHashOfBlock(h256 _hash) {
+dev::h256 BlockChainWrapper::parentHashOfBlock(h256 _hash)
+{
     return _hash;
 }
+}  // namespace eth
+}  // namespace dev

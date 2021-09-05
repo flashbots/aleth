6fa6da85deb441d4a5d8c92a40d146d59f501dce
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2021-09-05 12:41:01 -0400

    return basic string

--- libethereum/Executive.cpp
+++ libethereum/Executive.cpp
@@ -597,5 +597,5 @@ std::string StateWrapper::getRlp(h256 address)
     cout << " Value: ---- ";
     string ret = make_hex_string(value.begin(), value.end());
     cout << "Value ---- " << ret << "\n";
-    return ret;
+    return "f84d43890c22698d03605e5000a056e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421a0c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470";
 }

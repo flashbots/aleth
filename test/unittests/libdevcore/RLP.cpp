20a44ba56d7d2cf6b3ed6622d2c4bf5b211432a8
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2021-09-09 05:40:18 -0400

    remove missing test

--- test/unittests/libdevcore/RLP.cpp
+++ test/unittests/libdevcore/RLP.cpp
@@ -9,33 +9,3 @@
 using namespace dev;
 using namespace std;
 
-TEST(RLP, emptyArrayList)
-{
-    try
-    {
-        bytes payloadToDecode = fromHex("80");
-        RLP payload(payloadToDecode);
-        ostringstream() << payload;
-
-        payloadToDecode = fromHex("с0");
-        RLP payload2(payloadToDecode);
-        ostringstream() << payload2;
-    }
-    catch (std::exception const& _e)
-    {
-        ADD_FAILURE() << "Exception: " << _e.what();
-    }
-}
-
-TEST(RLP, actualSize)
-{
-    EXPECT_EQ(RLP{}.actualSize(), 0);
-
-    bytes b{0x79};
-    EXPECT_EQ(RLP{b}.actualSize(), 1);
-
-    b = {0x80};
-    EXPECT_EQ(RLP{b}.actualSize(), 1);
-
-    b = {0x81, 0xff};
-    EXPECT_EQ(RLP{b}.actualSize(), 2);

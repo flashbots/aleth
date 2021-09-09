2a8a7337cd7be4e33fd0a2e7be291c2d6aa6b41c
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2021-09-09 05:08:34 -0400

    Remove a lot of tests

--- test/unittests/libdevcrypto/MemTrie.h
+++ test/unittests/libdevcrypto/MemTrie.h
@@ -6,32 +6,3 @@
 
 #include <libdevcore/Common.h>
 #include <libdevcore/FixedHash.h>
-
-namespace dev
-{
-
-class MemTrieNode;
-
-/**
- * @brief Merkle Patricia Tree "Trie": a modifed base-16 Radix tree.
- */
-class MemTrie
-{
-public:
-	MemTrie(): m_root(nullptr) {}
-	~MemTrie();
-
-	h256 hash256() const;
-	bytes rlp() const;
-
-	void debugPrint();
-
-	std::string const& at(std::string const& _key) const;
-	void insert(std::string const& _key, std::string const& _value);
-	void remove(std::string const& _key);
-
-private:
-	MemTrieNode* m_root;
-};
-
-}
--- test/unittests/libethcore/BlockHeader.cpp
+++ test/unittests/libethcore/BlockHeader.cpp
@@ -8,12 +8,3 @@
 
 using namespace dev::eth;
 
-TEST(BlockHeader, isValid)
-{
-    BlockHeader header;
-    EXPECT_FALSE(header);
-    header.setTimestamp(0);
-    EXPECT_TRUE(header);
-    header.setTimestamp(-2);
-    EXPECT_FALSE(header);
-}

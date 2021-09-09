1378a37bc596dee872f864568e7f241c3ca3c967
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2021-09-09 05:08:06 -0400

    Remove body of a lot of tests

--- test/unittests/libethereum/StateUnitTests.cpp
+++ test/unittests/libethereum/StateUnitTests.cpp
@@ -20,75 +20,7 @@ namespace dev
 {
 namespace test
 {
-BOOST_FIXTURE_TEST_SUITE(StateUnitTests, TestOutputHelperFixture)
-
-BOOST_AUTO_TEST_CASE(Basic)
-{
-    Block s(Block::Null);
-}
-
-BOOST_AUTO_TEST_CASE(LoadAccountCode)
-{
-    Address addr{"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"};
-    State s{0};
-    s.createContract(addr);
-    uint8_t codeData[] = {'c', 'o', 'd', 'e'};
-    u256 version = 123;
-    s.setCode(addr, {std::begin(codeData), std::end(codeData)}, version);
-    s.commit(State::CommitBehaviour::RemoveEmptyAccounts);
-
-    auto& loadedCode = s.code(addr);
-    BOOST_CHECK(std::equal(std::begin(codeData), std::end(codeData), std::begin(loadedCode)));
-    BOOST_CHECK_EQUAL(s.version(addr), version);
-}
-
-BOOST_AUTO_TEST_CASE(RollbackSetCode)
-{
-    Address addr{"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"};
-    State s{0};
-    auto savepoint = s.savepoint();
-    s.createContract(addr);
-    uint8_t codeData[] = {'c', 'o', 'd', 'e'};
-    u256 version = 123;
-    s.setCode(addr, {std::begin(codeData), std::end(codeData)}, version);
-    s.rollback(savepoint);
-
-    BOOST_CHECK(!s.addressInUse(addr));
-    BOOST_CHECK(s.version(addr) == 0);
-
-    // only state root exists in DB
-    BOOST_CHECK_EQUAL(s.db().keys().size(), 1);
-}
-
-BOOST_AUTO_TEST_CASE(SetEmptyCode)
-{
-    Address addr{"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"};
-    State s{0};
-    s.createContract(addr);
-    s.setNonce(addr, 1);
-    s.setCode(addr, {}, 0);
-    s.commit(State::CommitBehaviour::RemoveEmptyAccounts);
-
-    BOOST_CHECK(!s.addressHasCode(addr));
-
-    // empty code is not saved to DB
-    BOOST_CHECK(!s.db().exists(EmptySHA3));
-}
-
-BOOST_AUTO_TEST_CASE(CodeVersionZero)
-{
-    Address addr{"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"};
-    State s{0};
-    s.createContract(addr);
-    uint8_t codeData[] = {'c', 'o', 'd', 'e'};
-    u256 version = 0;
-    s.setCode(addr, {std::begin(codeData), std::end(codeData)}, version);
-    s.commit(State::CommitBehaviour::RemoveEmptyAccounts);
-
-    auto& loadedCode = s.code(addr);
-    BOOST_CHECK(std::equal(std::begin(codeData), std::end(codeData), std::begin(loadedCode)));
-    BOOST_CHECK_EQUAL(s.version(addr), version);
-}
+BOOST_FIXTURE_TEST_SUITE(StateUnitTests, Test
 
 BOOST_AUTO_TEST_CASE(StateSetRootTest)
 {

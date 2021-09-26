5ca040f9b87f05c87d2f1b2f1c47398dc4c59777
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2021-09-26 19:18:47 -0400

    Add test

--- aleth/main.cpp
+++ aleth/main.cpp
@@ -2,9 +2,11 @@
 // Copyright 2015-2019 Aleth Authors.
 // Licensed under the GNU General Public License, Version 3.
 
+using namespace std;
 
 
 int main(int argc, char** argv)
 {
+    std::cout << "testing\n";
     return 0;
 }

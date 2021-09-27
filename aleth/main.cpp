f7f6e3186ff46a4d664c9004ba77b3497aa53571
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2021-09-27 16:46:10 -0400

    add basic rpc

--- aleth/main.cpp
+++ aleth/main.cpp
@@ -1,12 +1,25 @@
-// Aleth: Ethereum C++ client, tools and libraries.
-// Copyright 2015-2019 Aleth Authors.
-// Licensed under the GNU General Public License, Version 3.
-
 #include <iostream>
-using namespace std;
+#include "rpc/server.h"
 
-int main(/*int argc, char** argv*/)
-{
-    cout << "testing\n";
+void foo() {
+    std::cout << "foo was called!" << std::endl;
+}
+
+int main() {
+    // Creating a server that listens on port 8080
+    rpc::server srv(8080);
+
+    // Binding the name "foo" to free function foo.
+    // note: the signature is automatically captured
+    srv.bind("foo", &foo);
+
+    // Binding a lambda function to the name "add".
+    srv.bind("add", [](int a, int b) {
+        return a + b;
+    });
+
+    // Run the server loop.
+    srv.run();
+
     return 0;
 }

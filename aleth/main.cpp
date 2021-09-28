b00f9d2c5d6554e159f02ca7c8b9c5b212840842
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2021-09-28 02:43:55 -0400

    Fix main imports

--- aleth/main.cpp
+++ aleth/main.cpp
@@ -1,10 +1,11 @@
 #include "libweb3jsonrpc/Server.h"
+#include <jsonrpccpp/server/connectors/httpserver.h>
 #include <iostream>
+
 int main()
 {
     HttpServer httpserver(8383);
-    ServerBase s(httpserver,
-        JSONRPC_SERVER_V1V2);  // hybrid server (json-rpc 1.0 & 2.0)
+    ServerBase s(httpserver, JSONRPC_SERVER_V1V2);  // hybrid server (json-rpc 1.0 & 2.0)
     s.StartListening();
     cout << "Hit enter to stop the server" << endl;
     getchar();
